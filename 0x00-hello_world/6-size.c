fb58110c22d5c4ec2bdd8ba16b39344c6c50689f
Ahmed Magdi <thenabag03@gmail.com>
2023-09-07 16:39:56 -0700

    6th script C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,16 @@
 /**
  * main - Entry point
  *
- * Description: C program using printf to print function size of various types
+ * Description: C program to print size of various types
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %u bytes(s)", sizeof(char));
-	printf("Size of an int: %u bytes(s)", sizeof(int));
-	printf("Size of a long int: %u bytes(s)", sizeof(long int));
-	printf("Size of a long long int: %u bytes(s)", sizeof(long long int));
-	printf("Size of a float: %u bytes(s)", sizeof(float));
+	printf("Size of a char: %u bytes(s)\n", sizeof(char));
+	printf("Size of an int: %u bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %u bytes(s)\n", sizeof(float));
 	return (0);
 }
