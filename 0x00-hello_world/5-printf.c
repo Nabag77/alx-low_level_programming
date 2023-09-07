da32e84b6ec66d989f5a57b3bef55476095ac79a
Ahmed Magdi <thenabag03@gmail.com>
2023-09-07 16:06:48 -0700

    5th script C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Description: A C program that uses the printf function
+ * Description: C program using the printf function
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
