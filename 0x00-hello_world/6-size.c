2ac9aec9c25203c8a2ca938eec41b3bcd96e17ba
Ahmed Magdi <thenabag03@gmail.com>
2023-09-07 16:35:56 -0700

    6th script C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	printf("Size of a char %u bytes(s)\n", sizeof(char));
+	printf("Size of a char: %u bytes(s)\n", sizeof(char));
 	printf("Size of an int: %u bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
