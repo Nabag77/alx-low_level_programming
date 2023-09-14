3619cae8e77374da050b47475b9dffa72ada8b31
Ahmed Magdi <thenabag03@gmail.com>
2023-09-14 14:12:58 -0700

    5th prog.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,14 @@ void more_numbers(void)
 	for (line = 1; line <= 10; line++)
 	{
 
-		for (num = '0'; num <= '14'; num++)
-			_putchar(num);
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar('1');
+				_putchar(num % 10 + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
