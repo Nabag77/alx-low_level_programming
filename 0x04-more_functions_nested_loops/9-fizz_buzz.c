3360ce789eee2f3233ecf8cce5e1b70fca3b27d0
Ahmed Magdi <thenabag03@gmail.com>
2023-09-14 15:29:55 -0700

    9th prog.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Fizzbuzz test
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int nm;
+
+	for (nm = 1; nm <= 100; nm++)
+	{
+		if (nm % 3 == 0 && nm % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (nm % 3 != 0 && nm % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (nm % 3 == 0 && nm % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
