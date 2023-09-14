9901041e41d7ce7a26781a405368b334f32bf5f3
Ahmed Magdi <thenabag03@gmail.com>
2023-09-14 13:44:39 -0700

    4th prog.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,11 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = '0'; (num <= '9' && !(num == '2' || num == '4')); num++)
+	for (num = '0';  num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			continue;
+	}
 		_putchar(num);
 	_putchar('\n');
 }
