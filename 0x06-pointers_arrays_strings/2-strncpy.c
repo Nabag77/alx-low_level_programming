4759393b428f0f9b2d71f45f458821e60691d420
Ahmed Magdi <thenabag03@gmail.com>
2023-09-21 13:16:21 -0700

    2nd function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,6 +17,9 @@ char *_strncpy(char *dest, char *src, int n)
 	for (cp = 0; cp < n && src[cp] != 0; cp++)
 		dest[cp] = src[cp];
 	while (cp < n)
+	{
 		dest[cp] = '\0';
+		cp++;
+	}
 	return (dest);
 }
